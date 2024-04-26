9610923e56db31997a8410dcd22bc80b80fe5e31
GalahadIII <89609656+GalahadIII@users.noreply.github.com>
2024-04-26 12:23:31 -0400

    Delete htdocs/data/connection.php

--- htdocs/data/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-function createPDO(): PDO {
-    $host = "localhost";
-    $dbName = "genesys";
-    $user = "root";
-    $passwd = "";
-    $charset = "utf8";
-    return new PDO("mysql:host=$host;dbname=$dbName;$charset", $user, $passwd);
-}
